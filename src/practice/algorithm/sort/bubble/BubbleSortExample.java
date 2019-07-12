226736e9d77751d2ed74a6448353300f4ece9f58
mmuzahid <muzahid.engr@gmail.com>
2019-07-12 22:43:05 +0600

    Added Bubble Sort Example

---
+++ src/practice/algorithm/sort/bubble/BubbleSortExample.java
@@ -0,0 +1,36 @@
+package practice.algorithm.sort.bubble;
+
+import java.util.Arrays;
+
+public class BubbleSortExample {
+
+	public static void main(String args[]) {
+		int[] ages = { 5, 1, 17, 3, 80, 35 };
+		doBubbleSortAsc(ages, true);
+		System.out.println(Arrays.toString(ages));
+	}
+
+	private static void doBubbleSortAsc(int[] items, boolean printStep) {
+		int swapTemp;
+		for (int i = 0; i < items.length; i++) {
+			for (int j = 0; j < items.length - 1; j++) {
+				if (printStep) {
+					System.out.printf("Phase:i=%d, j=%d %s", i, j, Arrays.toString(items));
+				}
+				if (items[j] > items[j + 1]) {
+					if (printStep) {
+						System.out.printf(" -> swap(item[%d]=%d, item[%d]=%d)", j, items[j], j + 1,
+								items[j + 1]);
+					}
+					swapTemp = items[j];
+					items[j] = items[j + 1];
+					items[j + 1] = swapTemp;
+				}
+				if (printStep) {
+					System.out.printf(" -> %s\n", Arrays.toString(items));
+				}
+			}
+		}
+	}
+
+}

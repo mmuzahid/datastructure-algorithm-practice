1a7cad0797e2029c8db2997a10bb0a04151d78a6
Md. Muzahidul Islam <muzahid.engr@gmail.com>
2020-10-26 09:29:10 +0600

    Update and rename FibonacciLastDigitsPattern.java to FibonacciUtils.java

--- src/practice/algorithm/Fibonacci/FibonacciUtils.java
+++ src/practice/algorithm/Fibonacci/FibonacciUtils.java
@@ -33,6 +33,37 @@ public class FibonacciLastDigitsPattern {
     	}
     	
     }
+ 
+    /**
+    * returns a reapeated frame of (fib % m) as mod result repeats
+    */
+    public static List<Long> getModFrame(long m) {
+		BigInteger f0 = BigInteger.ZERO;
+		BigInteger f1 = BigInteger.ONE;
+		BigInteger fib;
+
+		List<Long> frames = new ArrayList<Long>();
+		frames.add(0l);
+		frames.add(1l);
+		long prev = 1l;
+		while (true) {
+			fib = f0.add(f1);
+			f0 = f1;
+			f1 = fib;
+			long mod = fib.mod(new BigInteger(String.valueOf(m))).longValue();
+			frames.add(mod);
+
+			if (mod == 1 && prev == m - 1) {
+				break;
+			} else {
+				prev = mod;
+			}
+		}
+
+		return frames;
+
+	}
+	
     
     public static void main(String[] args) {
 		printFibSquareLastDigits(200);
